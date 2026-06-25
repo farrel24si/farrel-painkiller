ce8bc9209a5ed78fe11f510de52760ec5a23c580
Farrel24si <farrel24si@mahasiswa.pcr.ac.id>
2026-06-25 15:42:08 +0700

    PRD v1: add title attribute on gallery images for accessibility

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -475,6 +475,7 @@ const LandingPage = () => {
                     <img
                       src={src}
                       alt={`Gallery ${idx + 1}`}
+                      title={`Capella Hotel Gallery ${idx + 1}`}
                       className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-700"
                     />
                   </div>
